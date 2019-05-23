602dae4b738a3e3a2e74052d1b47583a8dfc9f3d
Sebastian Quintero <jsebastianquintero@gmail.com>
2019-05-22 23:59:34 -0500

    Parte grafica actividad registro terminada

--- app/src/main/java/proyectohastafinal/almac/myapplication/RegistroEstilista.java
+++ app/src/main/java/proyectohastafinal/almac/myapplication/RegistroEstilista.java
@@ -2,6 +2,7 @@ package proyectohastafinal.almac.myapplication;
 
 import android.app.TimePickerDialog;
 import android.support.annotation.NonNull;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -127,7 +128,7 @@ public class RegistroEstilista extends AppCompatActivity {
                    /* rtdb.getReference().child("Salon de belleza").child(spinnerSalonesDeBelleza.getSelectedItem().toString()).child("servicios").addListenerForSingleValueEvent(new ValueEventListener() {
                         @Override
                         public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                            HashMap<String, Boolean> nombreSalonDeBelleza =  (HashMap<String, Boolean>) dataSnapshot.getValue();
+                            HashMap<String, Booleazn> nombreSalonDeBelleza =  (HashMap<String, Boolean>) dataSnapshot.getValue();
                             Log.d("nombreSalonDeBelleza", nombreSalonDeBelleza.keySet().toString() + " ");
 
                             servicios.addAll(nombreSalonDeBelleza.keySet());
