9b1a16e5599ba9beaf67147365ce33189c437ce1
Sebastian Quintero <jsebastianquintero@gmail.com>
2019-06-03 15:42:02 -0500

    Modelo cambiado y algunos errores arreglados

--- app/src/main/java/proyectohastafinal/almac/myapplication/CitasSalonFragment.java
+++ app/src/main/java/proyectohastafinal/almac/myapplication/CitasSalonFragment.java
@@ -6,25 +6,21 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FirebaseStorage;
 
 import java.util.ArrayList;
 
 import proyectohastafinal.almac.myapplication.model.Cita;
-import proyectohastafinal.almac.myapplication.model.FotoCatalogo;
 
-import static proyectohastafinal.almac.myapplication.model.ConsutaFirebase.*;
+import static proyectohastafinal.almac.myapplication.model.Constantes.*;
 
 public class CitasSalonFragment extends Fragment {
 
