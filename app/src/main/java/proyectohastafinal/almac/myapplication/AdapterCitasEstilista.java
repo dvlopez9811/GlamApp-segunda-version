50c1779a49b9ed8ff9243661f9b8ff9cdb32b360
ManuelCoral1998 <43486285+ManuelCoral1998@users.noreply.github.com>
2019-06-02 19:54:21 -0500

    Citas estilista

--- app/src/main/java/proyectohastafinal/almac/myapplication/AdapterCitasEstilista.java
+++ app/src/main/java/proyectohastafinal/almac/myapplication/AdapterCitasEstilista.java
@@ -1,5 +1,6 @@
 package proyectohastafinal.almac.myapplication;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -9,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -27,6 +29,7 @@ public class AdapterCitasEstilista extends RecyclerView.Adapter<AdapterCitasEsti
 
     ArrayList<Cita> citas;
     FirebaseDatabase rtdb;
+    Context context;
 
 
 //    public void agregarcita(Cita cita){
@@ -34,71 +37,143 @@ public class AdapterCitasEstilista extends RecyclerView.Adapter<AdapterCitasEsti
 //        notifyDataSetChanged();
 //    }
 
-    public static class CustomViewHolder extends RecyclerView.ViewHolder {
-        public LinearLayout root;
-        public CustomViewHolder(LinearLayout v) {
-            super(v);
-            root = v;
-        }
-    }
+//    public static class CustomViewHolder extends RecyclerView.ViewHolder {
+//        public LinearLayout root;
+//        public CustomViewHolder(LinearLayout v) {
+//            super(v);
+//            root = v;
+//        }
+//
+//    }
 
-    public AdapterCitasEstilista(){
-        rtdb = FirebaseDatabase.getInstance();
-        citas = new ArrayList<>();
+//    public AdapterCitasEstilista(){
+//        rtdb = FirebaseDatabase.getInstance();
+//        citas = new ArrayList<>();
+//    }
+
+    public AdapterCitasEstilista(Context context, ArrayList<Cita> citas){
+        this.context = context;
+        this.citas = citas;
     }
 
     @Override
     public CustomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        LinearLayout v = (LinearLayout) LayoutInflater.from(parent.getContext()).inflate(R.layout.renglon_cita_estilista, parent, false);
-        CustomViewHolder vh = new CustomViewHolder(v);
-        return vh;
+       return new CustomViewHolder(LayoutInflater.from(context).inflate(R.layout.renglon_cita_estilista, parent, false));
     }
 
     @Override
     public void onBindViewHolder(final CustomViewHolder holder, final int position) {
 
-        Cita aux = citas.get(position);
+        int horarioinic = citas.get(position).getHorainicio();
+        String horarioinicio = "";
+        if(citas.get(position).getHorainicio()<12){
+            horarioinicio=horarioinic+"a.m";
+        }
+        else {
+            if(horarioinic!=12)
+                horarioinic-=12;
+            horarioinicio=horarioinic+" p.m";
+        }
 
-        Log.d("CITASSS", aux.getIdUsuario() + "");
+        //StorageReference ref = storage.getReference().child("estilistas").child(citas.get(position).getIdEstilista());
+        //ImageView image =  holder.root.findViewById(R.id.image_cita_estilista);
+        //ref.getDownloadUrl().addOnSuccessListener(uri -> Glide.with(image.getContext()).load(uri).into(image));
+        holder.salon_renglon_cita_fragment_estilista_hoy.setText(citas.get(position).getNombreSalon());
+        holder.servicio_renglon_cita_fragment_estilista_hoy.setText(citas.get(position).getServicio());
+        holder.horainicio_renglon_cita_fragment_estilista_hoy.setText(horarioinicio);
+        holder.nombre_estilista_cita_fragment_estilista_hoy.setText("arreglar estilista");
+        holder.relative_renglon_cita_header_fragment_estilista_hoy.setVisibility(View.GONE);
 
-        rtdb.getReference().child("usuario").child(citas.get(position).getIdUsuario()).addListenerForSingleValueEvent(new ValueEventListener() {
+        if(citas.get(position).getInformacion().equals("CITAS POR CALIFICAR")) {
+            holder.relative_renglon_cita_header_fragment_estilista_hoy.setVisibility(View.VISIBLE);
+            holder.renglon_cita_fecha_fragment_estilista_hoy.setVisibility(TextView.GONE);
+            holder.renglon_cita_dias_restantes_fragment_estilista_hoy.setVisibility(TextView.GONE);
+            holder.renglon_cita_dia_de_la_semana_fragment_estilista_hoy.setText("CITAS POR CALIFICAR");
+        } else if(citas.get(position).getInformacion().equals("HOY")) {
+            holder.relative_renglon_cita_header_fragment_estilista_hoy.setVisibility(View.VISIBLE);
+            holder.renglon_cita_fecha_fragment_estilista_hoy.setText(citas.get(position).getFecha());
+            holder.renglon_cita_dias_restantes_fragment_estilista_hoy.setText("HOY");
+            holder.renglon_cita_dia_de_la_semana_fragment_estilista_hoy.setText(citas.get(position).getCabecera());
+        }else if(citas.get(position).getInformacion().equals("PRÓXIMO")) {
+            String[] datos = citas.get(position).getCabecera().split(" ");
+            holder.relative_renglon_cita_header_fragment_estilista_hoy.setVisibility(View.VISIBLE);
+            holder.renglon_cita_fecha_fragment_estilista_hoy.setText(citas.get(position).getFecha());
+            holder.renglon_cita_dias_restantes_fragment_estilista_hoy.setText(datos[1] + " dias");
+            holder.renglon_cita_dia_de_la_semana_fragment_estilista_hoy.setText(datos[0]);
+        }
+
+        holder.iv_menu_cita_renglon_cita_fragment_estilista_hoy.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                Cliente cliente = dataSnapshot.getValue(Cliente.class);
-
-                ((TextView) holder.root.findViewById(R.id.usuario_renglon_cita_estilista)).setText(cliente.getUsuario());
-                ((TextView) holder.root.findViewById(R.id.servicio_renglon_cita_estilista)).setText(citas.get(position).getServicio());
-
-                //Hora inicio
-                int horarioinic = citas.get(position).getHorainicio();
-                String horarioinicio = "";
-                if(citas.get(position).getHorainicio()<12){
-                    horarioinicio=horarioinic+"a.m";
-                }
-                else {
-                    if(horarioinic!=12)
-                        horarioinic-=12;
-                    horarioinicio=horarioinic+" p.m";
-                }
-
-                ((TextView) holder.root.findViewById(R.id.horainicio_renglon_cita_estilista)).setText(horarioinicio);
-                final Button btn_cancelar_cita_estilista = holder.root.findViewById(R.id.btn_cancelar_renglon_cita_estilista);
-                btn_cancelar_cita_estilista.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        listener.onItemClick(citas.get(position));
-                    }
-                });
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
+            public void onClick(View v) {
+                Log.d("PASANDOOOOO", "PASANDO");
+                listener.onItemClick(v,citas.get(position));
             }
         });
 
+//        Cita aux = citas.get(position);
+//
+//        Log.d("CITASSS", aux.getIdUsuario() + "");
+//
+//        rtdb.getReference().child("Estilista").child(citas.get(position).getIdEstilista()).addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//
+//                /*Cliente cliente = dataSnapshot.getValue(Cliente.class);
+//
+//                ((TextView) holder.root.findViewById(R.id.usuario_renglon_cita_estilista)).setText(cliente.getUsuario());
+//                ((TextView) holder.root.findViewById(R.id.servicio_renglon_cita_estilista)).setText(citas.get(position).getServicio());
+//
+//                //Hora inicio
+//                int horarioinic = citas.get(position).getHorainicio();
+//                String horarioinicio = "";
+//                if(citas.get(position).getHorainicio()<12){
+//                    horarioinicio=horarioinic+"a.m";
+//                }
+//                else {
+//                    if(horarioinic!=12)
+//                        horarioinic-=12;
+//                    horarioinicio=horarioinic+" p.m";
+//                }
+//
+//                ((TextView) holder.root.findViewById(R.id.horainicio_renglon_cita_estilista)).setText(horarioinicio);
+//                final Button btn_cancelar_cita_estilista = holder.root.findViewById(R.id.btn_cancelar_renglon_cita_estilista);
+//                btn_cancelar_cita_estilista.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//
+//                        listener.onItemClick(citas.get(position));
+//                    }
+//                });*/
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
 
+    }
+
+    class CustomViewHolder extends RecyclerView.ViewHolder {
+
+        RelativeLayout relative_renglon_cita_header_fragment_estilista_hoy;
+        TextView salon_renglon_cita_fragment_estilista_hoy, nombre_estilista_cita_fragment_estilista_hoy, servicio_renglon_cita_fragment_estilista_hoy, horainicio_renglon_cita_fragment_estilista_hoy,
+                renglon_cita_dia_de_la_semana_fragment_estilista_hoy,renglon_cita_fecha_fragment_estilista_hoy,renglon_cita_dias_restantes_fragment_estilista_hoy;
+        ImageView iv_menu_cita_renglon_cita_fragment_estilista_hoy;
+
+        public CustomViewHolder(View itemView) {
+            super(itemView);
+            salon_renglon_cita_fragment_estilista_hoy = itemView.findViewById(R.id.salon_renglon_cita_fragment_estilista_hoy);
+            nombre_estilista_cita_fragment_estilista_hoy = itemView.findViewById(R.id.nombre_estilista_cita_fragment_estilista_hoy);
+            servicio_renglon_cita_fragment_estilista_hoy = itemView.findViewById(R.id.servicio_renglon_cita_fragment_estilista_hoy);
+            horainicio_renglon_cita_fragment_estilista_hoy = itemView.findViewById(R.id.horainicio_renglon_cita_fragment_estilista_hoy);
+            iv_menu_cita_renglon_cita_fragment_estilista_hoy = itemView.findViewById(R.id.iv_menu_cita_renglon_cita_fragment_estilista_hoy);
+            relative_renglon_cita_header_fragment_estilista_hoy = itemView.findViewById(R.id.relative_renglon_cita_header_fragment_estilista_hoy);
+            renglon_cita_dia_de_la_semana_fragment_estilista_hoy = itemView.findViewById(R.id.renglon_cita_dia_de_la_semana_fragment_estilista_hoy);
+            renglon_cita_fecha_fragment_estilista_hoy = itemView.findViewById(R.id.renglon_cita_fecha_fragment_estilista_hoy);
+            renglon_cita_dias_restantes_fragment_estilista_hoy = itemView.findViewById(R.id.renglon_cita_dias_restantes_fragment_estilista_hoy);
+
+        }
     }
 
     @Override
@@ -112,16 +187,26 @@ public class AdapterCitasEstilista extends RecyclerView.Adapter<AdapterCitasEsti
         }
         notifyDataSetChanged();
     }
-
     //OBSERVER
-    public interface OnItemClickListener{
-        void onItemClick(Cita cita);
+    public interface OnItemClickListenerHoy{
+        void onItemClick(View v,Cita cita);
     }
 
-    private OnItemClickListener listener;
+    private OnItemClickListenerHoy listener;
 
-    public void setListener(OnItemClickListener listener){
+    public void setListener(OnItemClickListenerHoy listener){
         this.listener = listener;
     }
+
+//    //OBSERVER
+//    public interface OnItemClickListener{
+//        void onItemClick(Cita cita);
+//    }
+//
+//    private OnItemClickListener listener;
+//
+//    public void setListener(OnItemClickListener listener){
+//        this.listener = listener;
+//    }
 
 }
--- app/src/main/java/proyectohastafinal/almac/myapplication/SolicitudesEstilistaFragment.java
+++ app/src/main/java/proyectohastafinal/almac/myapplication/SolicitudesEstilistaFragment.java
@@ -1,17 +1,52 @@
 package proyectohastafinal.almac.myapplication;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.GregorianCalendar;
+import java.util.concurrent.TimeUnit;
+
+import proyectohastafinal.almac.myapplication.model.Cita;
+import proyectohastafinal.almac.myapplication.model.Estilista;
 
 
+public class SolicitudesEstilistaFragment extends Fragment implements AdapterCitasEstilista.OnItemClickListenerHoy{
 
-public class SolicitudesEstilistaFragment extends Fragment {
+    private AdapterCitasEstilista adapterCitasEstilista;
+    private RecyclerView lista_citas_estilista;
+    private ArrayList<Cita> citas;
+    private Cita citaseleccionada;
 
+    FirebaseDatabase rtdb;
+    FirebaseAuth auth;
+    ArrayList<Cita> citasEstilista;
+    Calendar calendario;
 
     private static SolicitudesEstilistaFragment instance;
 
@@ -33,6 +68,201 @@ public class SolicitudesEstilistaFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_solicitudes_estilista, container, false);
+        View v = inflater.inflate(R.layout.fragment_solicitudes_estilista, container, false);
+        auth = FirebaseAuth.getInstance();
+
+        calendario = Calendar.getInstance();
+
+        if (auth.getCurrentUser() == null) {
+            return v;
+        }
+
+        rtdb = FirebaseDatabase.getInstance();
+
+        lista_citas_estilista = v.findViewById(R.id.lista_citas_estilista_solicitudes);
+
+        citasEstilista = new ArrayList<>();
+        rtdb.getReference().child("Estilista").child(auth.getCurrentUser().getUid()).child("citas").addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Long[] size = new Long[1];
+                size[0] = dataSnapshot.getChildrenCount();
+                for (DataSnapshot hijo : dataSnapshot.getChildren()) {
+                    Query query = rtdb.getReference().child("Citas").child(hijo.getValue().toString());
+                    query.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            Cita cita = (dataSnapshot.getValue(Cita.class));
+                            String[] fechaCita = cita.getFecha().split("-");
+                            Calendar calendarioCita = new GregorianCalendar(Integer.parseInt(fechaCita[0]), Integer.parseInt(fechaCita[1]) - 1, Integer.parseInt(fechaCita[2]), cita.getHorainicio(), 0, 0);
+
+                            Calendar calendarioDiaAnterior = Calendar.getInstance();
+                            calendarioDiaAnterior.add(Calendar.DATE, -1);
+
+
+                            if (calendarioCita.getTimeInMillis() <= calendarioDiaAnterior.getTimeInMillis()) {
+                                //Eliminar la cita
+                                Log.e("CITA", calendarioCita.getTimeInMillis() + "");
+                                Log.e("ANTERIOR", calendarioDiaAnterior.getTimeInMillis() + "");
+                                size[0]--;
+                            } else
+                                citasEstilista.add(cita);
+                            if (citasEstilista.size() == size[0]) adapter();
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+        return v;
+
+
     }
+
+    private void adapter() {
+        Collections.sort(citasEstilista);
+
+        Calendar calendarioActual = Calendar.getInstance();
+        ArrayList<Cita> citasAux = new ArrayList<>();
+
+        for (int i = 0; i < citasEstilista.size(); i++) {
+            Cita cita = citasEstilista.get(i);
+            String[] fechaCita = cita.getFecha().split("-");
+            Calendar calendarioCita = new GregorianCalendar(Integer.parseInt(fechaCita[0]), Integer.parseInt(fechaCita[1]) - 1, Integer.parseInt(fechaCita[2]), cita.getHorainicio(), 0, 0);
+            if (calendarioCita.getTimeInMillis() < calendarioActual.getTimeInMillis()) {
+
+            } else {
+                if (calendarioActual.get(Calendar.DAY_OF_MONTH) == Integer.parseInt(fechaCita[2])) {
+                } else {
+                    long diff = calendarioCita.getTime().getTime() - calendarioActual.getTime().getTime();
+                    long diferencia = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+                    if (i == 0) {
+                        cita.setInformacion("PRÓXIMO");
+                        cita.setCabecera(cita.getDia().toUpperCase() + " " + diferencia);
+                    } else if (Integer.parseInt((citasEstilista.get(i - 1).getFecha().split("-"))[2]) != Integer.parseInt((cita.getFecha().split("-"))[2])) {
+                        cita.setInformacion("PRÓXIMO");
+                        cita.setCabecera(cita.getDia().toUpperCase() + " " + diferencia);
+                    }
+                    citasAux.add(citasEstilista.get(i));
+                }
+            }
+        }
+
+        adapterCitasEstilista = new AdapterCitasEstilista(getContext(), citasAux);
+        adapterCitasEstilista.setListener(SolicitudesEstilistaFragment.this);
+        lista_citas_estilista.setLayoutManager(new LinearLayoutManager(getContext()));
+        lista_citas_estilista.setAdapter(adapterCitasEstilista);
+        lista_citas_estilista.setHasFixedSize(true);
+    }
+
+    @Override
+    public void onItemClick(View v, Cita cita) {
+        this.registerForContextMenu(v);
+        getActivity().openContextMenu(v);
+        citaseleccionada = cita;
+        Log.d("Seleccionada", citaseleccionada+"");
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getActivity().getMenuInflater().inflate(R.menu.context_menu_citas_estilista,menu);
+    }
+
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.envio_mensaje_usuario_cita:
+
+                rtdb.getReference().child("Estilista").child(citaseleccionada.getIdEstilista()).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        Estilista estilista = dataSnapshot.getValue(Estilista.class);
+
+                        //Vamos a abrir la ventana de chat
+                        Intent i = new Intent(getActivity(),ChatActivity.class);
+                        i.putExtra("telEstilista", estilista.getTelefono());
+                        startActivity(i);
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+                break;
+
+            case R.id.cancelar_cita_desde_estilista:
+                AlertDialog.Builder dialogo1 = new AlertDialog.Builder(getActivity());
+                dialogo1.setTitle("Cita");
+                dialogo1.setMessage("¿ Esta seguro de cancelar la cita ?");
+                dialogo1.setCancelable(false);
+                dialogo1.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogo1, int id) {
+
+                        String idEstilista = citaseleccionada.getIdEstilista();
+                        String idUsuario = citaseleccionada.getIdUsuario();
+                        String idCita = citaseleccionada.getIdcita();
+                        String fecha = citaseleccionada.getFecha();
+                        String inicio = citaseleccionada.getHorainicio()+"";
+                        Log.e("idCIta", idCita +"");
+                        final boolean[] seElimino = {false};
+                        rtdb.getReference().child("Citas").child(idCita).removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.e("SIRVE", "elimino " + idCita);
+                            }
+                        });
+                        rtdb.getReference().child("Estilista").child(idEstilista).child("citas").child(idCita).removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.e("SIRVE2", "elimino " + idCita);
+                            }
+                        });
+
+                        rtdb.getReference().child("Estilista").child(idEstilista).child("agenda").child(fecha).child("horas").child(inicio).removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.e("SIRVE3", "elimino " + idCita);
+                            }
+                        });
+
+                        rtdb.getReference().child("usuario").child(idUsuario).child("citas").child(idCita).removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.e("SIRVE4", "elimino " + idCita);
+                            }
+                        });
+
+                        Toast.makeText(getActivity(),"Se cancelo esta cita",Toast.LENGTH_LONG).show();
+                    }
+                });
+                dialogo1.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogo1, int id) {
+                        Toast.makeText(getContext(), "ESTE fue el elegido pero de solicitudes", Toast.LENGTH_LONG).show();
+
+                    }
+                });
+                dialogo1.show();
+
+
+                break;
+
+        }
+
+        return super.onContextItemSelected(item);
+    }
+
 }
